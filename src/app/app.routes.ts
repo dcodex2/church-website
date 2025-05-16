db3a7c0c73d384f65518bd26bdccf930f962ddd9
Darren Santana <dcodexperience@gmail.com>
2025-05-16 13:24:52 -0400

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { ComponentsComponent } from './components/components/components.component';
-import { CarouselsComponent } from './components/carousels/carousels.component';
 import { InfoCardsComponent } from './components/info-cards/info-cards.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
-import { DropdownsComponent } from './components/dropdowns/dropdowns.component';
-import { VideoComponent } from './components/video/video.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { VisitComponent } from './components/visit/visit.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
@@ -29,28 +24,8 @@ export const routes: Routes = [
     component: ContactComponent,
   },
   {
-    path: 'components',
-    component: ComponentsComponent,
-  },
-  {
-    path: 'carousels',
-    component: CarouselsComponent,
-  },
-  {
     path: 'info-cards',
     component: InfoCardsComponent,
-  },
-  {
-    path: 'buttons',
-    component: ButtonsComponent,
-  },
-  {
-    path: 'dropdowns',
-    component: DropdownsComponent,
-  },
-  {
-    path: 'videos',
-    component: VideoComponent,
   },
   {
     path: 'calendar',
