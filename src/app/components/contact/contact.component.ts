0ba080119a4f9dcfa8bf3ae6aca5441cccd30cb9
Darren Santana <dcodexperience@gmail.com>
2025-05-17 16:53:55 -0400

    fix

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../shared/components/button/button.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   provideFunctions,
   getFunctions,
   httpsCallable,
+  Functions,
 } from '@angular/fire/functions';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, TranslateModule],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    TranslateModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
   contactForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private functions: Functions,
+    private translate: TranslateService
+  ) {}
   ngOnInit(): void {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
@@ -27,23 +43,21 @@ export class ContactComponent {
     });
   }
 
-  submitContactForm() {
-    if (this.contactForm.valid) {
-      const { name, email, message } = this.contactForm.value;
-      const functions = getFunctions();
-      const sendContactEmail = httpsCallable(functions, 'sendContactEmail');
+  onSubmit() {
+    if (this.contactForm.invalid) return;
 
-      sendContactEmail({ name, email, message })
-        .then(() => {
-          alert('✅ Message sent successfully!');
-          this.contactForm.reset();
-        })
-        .catch((error) => {
-          console.error('Email error:', error);
-          alert('❌ Failed to send message. Please try again later.');
-        });
-    } else {
-      this.contactForm.markAllAsTouched();
-    }
+    const formData = this.contactForm.value;
+
+    const sendContactEmail = httpsCallable(this.functions, 'sendContactEmail');
+
+    sendContactEmail(formData)
+      .then(() => {
+        alert('Message sent!');
+        this.contactForm.reset();
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Failed to send message.');
+      });
   }
 }
